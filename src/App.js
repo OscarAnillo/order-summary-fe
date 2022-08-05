d0e93892bb605759eb104155639c47947daa7d94
oscar anillo <oscarani14@gmail.com>
2022-08-05 16:21:48 -0500

    add image and text component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CardComponent from './Components/card';
 
 function App() {
   return (
     <div className="App">
       <h1>Oscar Anillo</h1>
       <p>Web UI Developer</p>
+      <CardComponent />
     </div>
   );
 }
